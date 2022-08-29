0128d321fa902cc96cc60f01a747be971f5404e8
n00mkrad <login@nmkd.de>
2022-08-29 10:58:19 +0200

    delete src folder after setting up env

--- install.cmd
+++ install.cmd
@@ -10,4 +10,5 @@ call "%CONDA_PATH%\activate.bat"
 call conda env create -f environment.yaml
 call conda env update --file environment.yaml --prune
 call "%CONDA_PATH%\activate.bat" ldo
-rem python "%CD%"\scripts\relauncher.py
+
+rmdir /s /q src
