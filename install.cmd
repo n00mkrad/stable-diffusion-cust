b516c9c8df5a930e9cbe27f88f596f856c400c00
n00mkrad <login@nmkd.de>
2022-08-29 11:20:16 +0200

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,11 +1,15 @@
 @echo off
 
+echo.
 echo Downloading Stable Diffusion 1.4 model file...
+
 curl "https://drive.yerf.org/wl/?id=EBfTrmcCCUAGaQBXVIj5lJmEhjoP1tgl&mode=grid&download=1" -o model.ckpt
 
 SET CONDA_PATH=..\mc\Scripts
 
+echo.
 echo Setting up python environment...
+
 call "%CONDA_PATH%\activate.bat"
 call conda env create -f environment.yaml
 call conda env update --file environment.yaml --prune
