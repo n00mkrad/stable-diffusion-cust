2d059866195233cd74517fd92ab98a55aabc4690
n00mkrad <login@nmkd.de>
2022-08-29 10:45:47 +0200

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,22 +1,11 @@
 @echo off
 
+echo Downloading Stable Diffusion 1.4 model file...
 curl "https://drive.yerf.org/wl/?id=EBfTrmcCCUAGaQBXVIj5lJmEhjoP1tgl&mode=grid&download=1" -o model.ckpt
 
-set paths=..\mc\Scripts
+SET CONDA_PATH=..\mc\Scripts
 
-for %%a in (%paths%) do ( 
- if EXIST "%%a\activate.bat" (
-    SET CONDA_PATH=%%a
- )
-)
-
-IF "%CONDA_PATH%"=="" (
-  echo anaconda3/miniconda3 not found. Install from here https://docs.conda.io/en/latest/miniconda.html
-  exit /b 1 
-) else (
-  echo anaconda3/miniconda3 detected in %CONDA_PATH%
-)
-
+echo Setting up python environment...
 call "%CONDA_PATH%\activate.bat"
 call conda env create -f environment.yaml
 call conda env update --file environment.yaml --prune
