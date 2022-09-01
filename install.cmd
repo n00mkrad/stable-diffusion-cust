7bf57dd2a35301f85c68211af004fda254211d1d
n00mkrad <login@nmkd.de>
2022-09-01 23:56:16 +0200

    fix messed up paths

--- install.cmd
+++ install.cmd
@@ -1,11 +1,16 @@
 @echo off
 
-SET CONDA_PATH=..\mc\Scripts
+SET CONDA_ROOT_PATH=..\mc
+SET CONDA_SCRIPTS_PATH=..\mc\Scripts
+
+SET PATH=..\mc\;%PATH%
+SET PATH=..\mc\Scripts;%PATH%
+SET PATH=..\mc\Library\bin;%PATH%
 
 echo.
 echo PRINTME Setting up python environment...
 
-call "%CONDA_PATH%\activate.bat"
-call conda env create -f environment.yaml -p "%CONDA_PATH%\envs\ldo"
-call conda env update --file environment.yaml --prune -p "%CONDA_PATH%\envs\ldo"
-call "%CONDA_PATH%\activate.bat" "%CONDA_PATH%\envs\ldo"
+call "%CONDA_SCRIPTS_PATH%\activate.bat"
+call conda env create -f environment.yaml -p "%CONDA_ROOT_PATH%\envs\ldo"
+call conda env update --file environment.yaml --prune -p "%CONDA_ROOT_PATH%\envs\ldo"
+call "%CONDA_SCRIPTS_PATH%\activate.bat" "%CONDA_ROOT_PATH%\envs\ldo"
