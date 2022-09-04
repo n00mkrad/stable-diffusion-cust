f82ffadc60b18c243567af0cc323b3e9f7b3e999
n00mkrad <login@nmkd.de>
2022-09-04 11:38:37 +0200

    update install script to use micromamba

--- install.cmd
+++ install.cmd
@@ -1,16 +1,16 @@
 @echo off
 
-SET CONDA_ROOT_PATH=..\mc
-SET CONDA_SCRIPTS_PATH=..\mc\Scripts
+SET CONDA_ROOT_PATH=..\mb
+SET CONDA_SCRIPTS_PATH=..\mb\Scripts
 
-SET PATH=..\mc\;%PATH%
-SET PATH=..\mc\Scripts;%PATH%
-SET PATH=..\mc\Library\bin;%PATH%
+SET PATH=..\mb\condabin;%PATH%
+SET PATH=..\mb\Library\bin;%PATH%
+SET PATH=..\mb\Scripts;%PATH%
+SET PATH=..\mb\;%PATH%
 
 echo.
 echo PRINTME Setting up python environment...
 
-call "%CONDA_SCRIPTS_PATH%\activate.bat"
-call conda env create -f environment.yaml -p "%CONDA_ROOT_PATH%\envs\ldo"
-call conda env update --file environment.yaml --prune -p "%CONDA_ROOT_PATH%\envs\ldo"
+_conda env create -f environment.yaml -p "%CONDA_ROOT_PATH%\envs\ldo"
+_conda env update --file environment.yaml --prune -p "%CONDA_ROOT_PATH%\envs\ldo"
 call "%CONDA_SCRIPTS_PATH%\activate.bat" "%CONDA_ROOT_PATH%\envs\ldo"
