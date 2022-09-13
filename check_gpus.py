1678d408c844a4ae2b9cc4235e312e2dc61e1ece
n00mkrad <login@nmkd.de>
2022-09-13 13:07:17 +0200

    Update check_gpus.py

--- check_gpus.py
+++ check_gpus.py
@@ -1,4 +1,4 @@
 import torch
 
 for i in range(torch.cuda.device_count()):
-    print(f"{i} => {torch.cuda.get_device_name(i)}")
+    print(f"{i} - {torch.cuda.get_device_name(i)} - {round(torch.cuda.get_device_properties(0).total_memory / 1024 / 1024 / 1024, 2)} GB")
