769e7eaa6c2bf7c255a90b8c2d73e6039238dddd
n00mkrad <login@nmkd.de>
2022-09-30 12:29:29 +0200

    remove old commented-out code

--- scripts/merge_models.py
+++ scripts/merge_models.py
@@ -3,7 +3,6 @@ import argparse
 import torch
 from tqdm import tqdm
 
-
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
@@ -35,23 +34,12 @@ parser.add_argument(
 
 opt = parser.parse_args()
 
-
-# parser = argparse.ArgumentParser(description="Merge two models")
-# user_input_1 = input("Enter the first model name: ")
-# user_input_2 = input("Enter the second model name: ")
-# alpha = float(input("Enter the second model weight(default:0.5): ") or 0.5)
-# parser.add_argument("--output", type=str, help="Output file name, without extension", default="merged", required=False)
-
-# args = parser.parse_args()
-
 model_0 = torch.load(opt.model1)
 model_1 = torch.load(opt.model2)
 theta_0 = model_0["state_dict"]
 theta_1 = model_1["state_dict"]
 
 print("Merging models, please wait...")
-
-# output_file = f'{user_input_1.strip(".ckpt")}-{str(round((1-alpha)*100,2))}-{user_input_2.strip(".ckpt")}-{str(round((alpha*100),2))}.ckpt'
 
 for key in tqdm(theta_0.keys(), desc="Stage 1/2"):
     if "model" in key and key in theta_1:
