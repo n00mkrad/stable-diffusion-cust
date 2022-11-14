e8f70359f7d58984a46e79f8d96ebae40ee4d062
n00mkrad <login@nmkd.de>
2022-11-14 15:27:35 +0100

    allow token logging to fail

--- ldm/invoke/prompt_parser.py
+++ ldm/invoke/prompt_parser.py
@@ -686,17 +686,20 @@ def log_tokenization(text, model, display_label=None):
     discarded = ""
     usedTokens = 0
     totalTokens = len(tokens)
-    for i in range(0, totalTokens):
-        token = tokens[i].replace('</w>', 'x` ')
-        # alternate color
-        s = (usedTokens % 6) + 1
-        if i < model.cond_stage_model.max_length:
-            tokenized = tokenized + f"\x1b[0;3{s};40m{token}"
-            usedTokens += 1
-        else:  # over max token length
-            discarded = discarded + f"\x1b[0;3{s};40m{token}"
-    print(f"\n>> Tokens {display_label or ''} ({usedTokens}):\n{tokenized}\x1b[0m")
-    if discarded != "":
-        print(
-            f">> Tokens Discarded ({totalTokens-usedTokens}):\n{discarded}\x1b[0m"
-        )
+    try:
+        for i in range(0, totalTokens):
+            token = tokens[i].replace('</w>', 'x` ')
+            # alternate color
+            s = (usedTokens % 6) + 1
+            if i < model.cond_stage_model.max_length:
+                tokenized = tokenized + f"\x1b[0;3{s};40m{token}"
+                usedTokens += 1
+            else:  # over max token length
+                discarded = discarded + f"\x1b[0;3{s};40m{token}"
+        print(f"\n>> Tokens {display_label or ''} ({usedTokens}):\n{tokenized}\x1b[0m")
+        if discarded != "":
+            print(
+                f">> Tokens Discarded ({totalTokens-usedTokens}):\n{discarded}\x1b[0m"
+            )
+    except:
+        pass
