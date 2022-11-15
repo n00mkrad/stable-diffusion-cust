d63bfdcbe10ff785f2d01af63a22e9481f8355a7
n00mkrad <login@nmkd.de>
2022-11-15 03:45:17 +0100

    update venv deps

--- install-venv-deps.bat
+++ install-venv-deps.bat
@@ -1,24 +1,24 @@
-pip install numpy==1.23.4 --force-reinstall
-pip install albumentations==0.4.3 --force-reinstall
-pip install opencv-python==4.5.5.64 --force-reinstall
-pip install pudb==2019.2 --force-reinstall
-pip install imageio==2.9.0 --force-reinstall
-pip install pytorch-lightning==1.6.5 --force-reinstall
-pip install omegaconf==2.1.1 --force-reinstall
-pip install test-tube>=0.7.5 --force-reinstall
-pip install streamlit==1.12.0 --force-reinstall
-pip install send2trash==1.8.0 --force-reinstall
-pip install pillow==9.2.0 --force-reinstall
-pip install einops==0.3.0 --force-reinstall
-pip install torch-fidelity==0.3.0 --force-reinstall
-pip install transformers==4.19.2 --force-reinstall
-pip install torchmetrics==0.6.0 --force-reinstall
-pip install kornia==0.6.0 --force-reinstall
-pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip --force-reinstall
-pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers --force-reinstall
-pip install -e git+https://github.com/Birch-san/k-diffusion.git@mps#egg=k_diffusion --force-reinstall
-pip install -e git+https://github.com/invoke-ai/Real-ESRGAN.git#egg=realesrgan --force-reinstall
-pip install -e git+https://github.com/invoke-ai/GFPGAN.git#egg=gfpgan --force-reinstall
-pip install -e git+https://github.com/invoke-ai/clipseg.git@models-rename#egg=clipseg --force-reinstall
-pip install -e ./repo --force-reinstall
-pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116 --force-reinstall
+pip install numpy==1.23.4
+pip install albumentations==0.4.3
+pip install opencv-python==4.5.5.64
+pip install pudb==2019.2
+pip install imageio==2.9.0
+pip install pytorch-lightning==1.6.5
+pip install omegaconf==2.1.1
+pip install test-tube>=0.7.5
+pip install streamlit==1.12.0
+pip install send2trash==1.8.0
+pip install pillow==9.2.0
+pip install einops==0.3.0
+pip install torch-fidelity==0.3.0
+pip install transformers==4.19.2
+pip install torchmetrics==0.6.0
+pip install kornia==0.6.0
+pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
+pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
+pip install -e git+https://github.com/Birch-san/k-diffusion.git@mps#egg=k_diffusion
+pip install -e git+https://github.com/invoke-ai/Real-ESRGAN.git#egg=realesrgan
+pip install -e git+https://github.com/invoke-ai/GFPGAN.git#egg=gfpgan
+pip install -e git+https://github.com/invoke-ai/clipseg.git@models-rename#egg=clipseg
+pip install -e ./repo
+pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
