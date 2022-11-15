0bb5e1de947ce59c8294b0498e264635ee063ce3
n00mkrad <login@nmkd.de>
2022-11-15 15:39:10 +0100

    prefix python -m

--- install-venv-deps-onnx.bat
+++ install-venv-deps-onnx.bat
@@ -1,5 +1,5 @@
-pip install diffusers==0.6.0
-pip install onnx
-pip install onnxruntime
-pip install onnxruntime-directml --force-reinstall
-pip install protobuf==3.20.1 --force-reinstall
+python -m pip install diffusers==0.6.0
+python -m pip install onnx
+python -m pip install onnxruntime
+python -m pip install onnxruntime-directml --force-reinstall
+python -m pip install protobuf==3.20.1 --force-reinstall
--- install-venv-deps.bat
+++ install-venv-deps.bat
@@ -1,24 +1,24 @@
-pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
-pip install numpy==1.23.4
-pip install albumentations==0.4.3
-pip install opencv-python==4.5.5.64
-pip install pudb==2019.2
-pip install imageio==2.9.0
-pip install pytorch-lightning==1.6.5
-pip install omegaconf==2.1.1
-pip install test-tube>=0.7.5
-pip install streamlit==1.12.0
-pip install send2trash==1.8.0
-pip install pillow==9.2.0
-pip install einops==0.3.0
-pip install torch-fidelity==0.3.0
-pip install transformers==4.19.2
-pip install torchmetrics==0.6.0
-pip install kornia==0.6.0
-pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
-pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
-pip install -e git+https://github.com/Birch-san/k-diffusion.git@mps#egg=k_diffusion
-pip install -e git+https://github.com/invoke-ai/Real-ESRGAN.git#egg=realesrgan
-pip install -e git+https://github.com/invoke-ai/GFPGAN.git#egg=gfpgan
-pip install -e git+https://github.com/invoke-ai/clipseg.git@models-rename#egg=clipseg
-pip install -e ./repo
+python -m pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
+python -m pip install numpy==1.23.4
+python -m pip install albumentations==0.4.3
+python -m pip install opencv-python==4.5.5.64
+python -m pip install pudb==2019.2
+python -m pip install imageio==2.9.0
+python -m pip install pytorch-lightning==1.6.5
+python -m pip install omegaconf==2.1.1
+python -m pip install test-tube>=0.7.5
+python -m pip install streamlit==1.12.0
+python -m pip install send2trash==1.8.0
+python -m pip install pillow==9.2.0
+python -m pip install einops==0.3.0
+python -m pip install torch-fidelity==0.3.0
+python -m pip install transformers==4.19.2
+python -m pip install torchmetrics==0.6.0
+python -m pip install kornia==0.6.0
+python -m pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
+python -m pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
+python -m pip install -e git+https://github.com/Birch-san/k-diffusion.git@mps#egg=k_diffusion
+python -m pip install -e git+https://github.com/invoke-ai/Real-ESRGAN.git#egg=realesrgan
+python -m pip install -e git+https://github.com/invoke-ai/GFPGAN.git#egg=gfpgan
+python -m pip install -e git+https://github.com/invoke-ai/clipseg.git@models-rename#egg=clipseg
+python -m pip install -e ./repo
