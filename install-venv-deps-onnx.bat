00a2556a24ad34313909c5d0bc8abd606c5affcf
n00mkrad <login@nmkd.de>
2022-11-15 03:20:41 +0100

    venv bat files

---
+++ install-venv-deps-onnx.bat
@@ -0,0 +1,5 @@
+pip install diffusers==0.6.0 --force-reinstall
+pip install onnx --force-reinstall
+pip install onnxruntime --force-reinstall
+pip install onnxruntime-directml --force-reinstall
+pip install protobuf==3.20.1 --force-reinstall
