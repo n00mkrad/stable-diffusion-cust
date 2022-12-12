9cbb400d4ec43b482ae640de170c8d90842fb99e
n00mkrad <login@nmkd.de>
2022-12-13 00:56:04 +0100

    Pin ONNX deps

--- install-venv-deps-onnx.bat
+++ install-venv-deps-onnx.bat
@@ -1,5 +1,5 @@
-python -m pip install onnx
-python -m pip install onnxruntime
-python -m pip install onnxruntime-directml --force-reinstall
+python -m pip install onnx==1.13.0
+python -m pip install onnxruntime==1.13.1
+python -m pip install onnxruntime-directml==1.13.1 --force-reinstall
 python -m pip install git+https://github.com/huggingface/diffusers@326de4191578dfb55cb968880d40d703075e331e
 python -m pip install protobuf==3.20.1
