cda5dfc32d366ecbb9b868cebbb20f3ce21f1f59
n00mkrad <login@nmkd.de>
2022-12-24 21:38:27 +0100

    Update install-venv-deps-onnx.bat

--- install-venv-deps-onnx.bat
+++ install-venv-deps-onnx.bat
@@ -3,3 +3,4 @@ python -m pip install onnxruntime==1.13.1
 python -m pip install onnxruntime-directml==1.13.1 --force-reinstall
 python -m pip install git+https://github.com/huggingface/diffusers@326de4191578dfb55cb968880d40d703075e331e
 python -m pip install protobuf==3.20.1
+python -m pip install numpy==1.23.4
