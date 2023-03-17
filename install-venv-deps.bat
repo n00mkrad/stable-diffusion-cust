4f98434fd4341caec8269fe523d9a3ec7df3437c
n00mkrad <login@nmkd.de>
2023-03-17 11:24:25 +0100

    Update install-venv-deps.bat

--- install-venv-deps.bat
+++ install-venv-deps.bat
@@ -14,12 +14,12 @@ python -m pip install torchmetrics==0.6.0
 python -m pip install kornia==0.6.0
 python -m pip install realesrgan==0.3.0
 python -m pip install picklescan==0.0.5
-python -m pip install safetensors==0.2.5
+python -m pip install safetensors~=0.3.0
 python -m pip install git+https://github.com/huggingface/diffusers@180841bbde4b200be43350164eef80c93a68983a
 python -m pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
 python -m pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
 python -m pip install git+https://github.com/Birch-san/k-diffusion.git@mps#egg=k_diffusion
 python -m pip install -e git+https://github.com/invoke-ai/GFPGAN.git#egg=gfpgan
 python -m pip install -e git+https://github.com/invoke-ai/clipseg.git@models-rename#egg=clipseg
-python -m pip install -e ./repo
+python -m pip install -e ./repo/invoke
 python -m pip install numpy==1.23.4
