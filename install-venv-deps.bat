4a13be2a0ff71f542cb0592641faa5bfe05558eb
n00mkrad <login@nmkd.de>
2023-05-22 18:05:10 +0200

    Update deps, remove unneeded packages

--- install-venv-deps.bat
+++ install-venv-deps.bat
@@ -9,9 +9,6 @@ python -m pip install albumentations==0.4.3
 echo --- Will now install: opencv-python ---
 python -m pip install opencv-python==4.5.5.64
 
-echo --- Will now install: pudb ---
-python -m pip install pudb==2019.2
-
 echo --- Will now install: imageio ---
 python -m pip install imageio==2.9.0
 
@@ -34,7 +31,7 @@ echo --- Will now install: torch-fidelity ---
 python -m pip install torch-fidelity==0.3.0
 
 echo --- Will now install: transformers ---
-python -m pip install transformers~=4.26
+python -m pip install transformers==4.29.2
 
 echo --- Will now install: torchmetrics ---
 python -m pip install torchmetrics==0.6.0
@@ -49,16 +46,16 @@ echo --- Will now install: safetensors ---
 python -m pip install safetensors~=0.3.0
 
 echo --- Will now install: diffusers ---
-python -m pip install git+https://github.com/huggingface/diffusers@6ba0efb9a188b08f5b46565a87c0b3da7ff46af4
+python -m pip install diffusers==0.15.1
 
 echo --- Will now install: clip ---
 python -m pip install git+https://github.com/openai/CLIP.git@main#egg=clip
 
 echo --- Will now install: taming-transformers ---
-python -m pip install git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
+python -m pip install taming-transformers-rom1504==0.0.6
 
 echo --- Will now install: k_diffusion ---
-python -m pip install git+https://github.com/Birch-san/k-diffusion.git@363386981fee88620709cf8f6f2eea167bd6cd74#egg=k_diffusion
+python -m pip install git+https://github.com/Birch-san/k-diffusion.git@b23db2229953b109e8559c9756545e42eaa0172e#egg=k_diffusion
 
 echo --- Will now install: gfpgan ---
 python -m pip install gfpgan==1.3.8
